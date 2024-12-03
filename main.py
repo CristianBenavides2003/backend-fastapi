1c5e2ae1b220fb8f116aa90782d4db04e80b29be
CristianBenavides <cdbenavides@uninorte.edu.co>
2024-12-02 21:15:15 -0500

    se agregan deployments

--- main.py
+++ main.py
@@ -27,23 +27,23 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post("/users/", response_model=schemas.UserResponse)
+@app.post("/fastapi/users/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
-@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+@app.get("/fastapi/users/{user_id}", response_model=schemas.UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-@app.get("/users/", response_model=list[schemas.UserResponse],)
+@app.get("/fastapi/users/", response_model=list[schemas.UserResponse],)
 
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_users(db, skip=skip, limit=limit)
 
-@app.delete("/users/{user_id}")
+@app.delete("/fastapi/users/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     user = crud.delete_user(db, user_id)
     if user is None:
